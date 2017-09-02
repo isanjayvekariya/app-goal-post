793d287da96fabbb022b330db5063fa29e37e1c6
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-01 22:48:11 -0400

    create goalvc

---
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,36 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Sanjay Vekariya on 9/1/17.
+//  Copyright © 2017 Sanjay Vekariya. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTxtView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nectBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func shortTermPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
