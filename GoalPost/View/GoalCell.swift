c700657f61ff3363932c594a4951487c142d5d56
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-01 20:46:34 -0400

    build goad cell

---
+++ GoalPost/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Sanjay Vekariya on 9/1/17.
+//  Copyright © 2017 Sanjay Vekariya. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    // Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+}
