e21529eee0a3b6ecc32dd48969f460927e79574f
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-01 22:16:49 -0400

    display static data in goal cell, tableview, enum

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -16,13 +16,30 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let goal = Goal()
-        goal.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
     
     @IBAction func addGoalBtnPressed(_ sender: Any) {
         print("Btn was pressed...")
     }
+}
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell", for: indexPath) as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salas twice a week.", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
 }
 
---
+++ GoalPost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Sanjay Vekariya on 9/1/17.
+//  Copyright © 2017 Sanjay Vekariya. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
