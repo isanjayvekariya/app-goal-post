7f7f2a9d841bcbe1f4a362e2d0cc83991e35c6ff
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-03 11:07:50 -0400

    saving goal data to persistant store

--- GoalPost/Controller/FinishGoalVC.swift
+++ GoalPost/Controller/FinishGoalVC.swift
@@ -29,7 +29,31 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func createGoalBtnPressed(_ sender: Any) {
-        
+        self.save { (success) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
     
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
+    func save(completion: @escaping (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        let goal = Goal(context: managedContext)
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTxtField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            completion(true)
+        } catch {
+            completion(false)
+        }
+    }
 }
