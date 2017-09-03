6b1deecc5b49530794c8ff2b3ccd27f4715f2bbd
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-03 15:02:32 -0400

    set goal progress, set tableview row/update

--- GoalPost/View/GoalCell.swift
+++ GoalPost/View/GoalCell.swift
@@ -14,10 +14,19 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
     
+    @IBOutlet weak var completeView: UIView!
+    
+    
     func configureCell(goal: Goal) {
         self.goalDescriptionLbl.text = goal.goalDescription
         self.goalTypeLbl.text = goal.goalType
         self.goalProgressLbl.text = String(describing: goal.goalProgress)
+        
+        if goal.goalProgress == goal.goalCompletionValue {
+            self.completeView.isHidden = false
+        } else {
+            self.completeView.isHidden = true
+        }
     }
     
 }
